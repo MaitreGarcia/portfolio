86c2bc8f1e5660e280940a362135e5eaacd02890
Garcia Canelo Léo <leo.garciacanelo@3ds.com>
2023-05-06 02:16:12 +0200

    Add Versel Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics';
 
 import App from './App.vue'
 import router from './router'
@@ -19,3 +20,6 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
+inject()
