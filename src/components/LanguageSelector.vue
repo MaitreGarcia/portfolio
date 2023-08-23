56c1c47d23f3a57a0f3faac05dec71aefa8c5840
Garcia Canelo Léo <leo.garciacanelo@3ds.com>
2023-08-23 15:05:14 +0200

    Correct compilations errors

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import { GlobeEuropeAfricaIcon } from '@heroicons/vue/24/solid'
-
-
 </script>
 <template>
-  <div class="flex flex-row gap-1 item-center cursor-pointer" @click="emit('displayMenu')">
-    <GlobeEuropeAfricaIcon class="w-7 h-7" />
+  <div class="flex flex-row gap-1 item-center cursor-pointer" @click="$emit('displayMenu')">
+    <GlobeEuropeAfricaIcon class="w-6 h-6" />
     FR
   </div>
 </template>
--- src/components/WorkInProgress.vue
+++ src/components/WorkInProgress.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import ExclamationTriangleIcon from '@heroicons/vue/24/solid';
+import { ExclamationTriangleIcon } from '@heroicons/vue/24/solid';
 </script>
 <template>
   <div class="text-orange-200 gap-2 animate-pulse duration-500">
