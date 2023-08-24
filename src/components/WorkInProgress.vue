ce391777ac2ffd9572735009af799993a38bd45c
Garcia Canelo Léo <leo.garciacanelo@3ds.com>
2023-08-24 13:32:42 +0200

    Remove work in progress color

--- src/components/WorkInProgress.vue
+++ src/components/WorkInProgress.vue
@@ -2,7 +2,7 @@
 import { ExclamationTriangleIcon } from '@heroicons/vue/24/solid';
 </script>
 <template>
-  <div class="flex flex-row align-center items-center text-orange-200 gap-1 animate-pulse duration-500">
+  <div class="flex flex-row align-center items-center gap-1 animate-pulse duration-500">
     <ExclamationTriangleIcon class="w-6 h-6" />
     Work in progress
   </div>
